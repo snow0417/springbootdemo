9b6b98f795fb0f00f9f365494970ccfef8f40646
xueyu <xueyu@ushareit.com>
2020-07-16 14:31:00 +0800

    change

--- src/main/java/com/example/demo/Client.java
+++ src/main/java/com/example/demo/Client.java
@@ -1,47 +1,31 @@
 package com.example.demo;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Set;
-import java.util.TreeSet;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * @author xueyu
  * @date 2020/5/21 14:43
  */
 public class Client {
-    private static Object o = new Object();
 
-    public static void main(String[] args) throws Exception {
-        int[] s = new int[1];
-        Queue<Integer> q = new LinkedList<>();
-        q.add(1);
-        q.add(2);
-
-        Integer p = q.peek();
-        Integer poll = q.poll();
-        Integer r = q.remove();
-
-        q.offer(23);
-        q.offer(233);
+·
+    public static void main(String[] args) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("country_tele_code", "+86");
+        map.put("phone_code", "18500361569");
+        map.put("sms_content", "1234");
+        map.put("trace_id", "test_123");
+        map.put("deliver_type", "rst_test");
+        map.put("shareit_id", "shareit_id");
+        map.put("user_id", "userid");
+        map.put("app_version", "4040548");
+        map.put("channel_code", "beelink");
+        map.put("user_request_id", "user_request_id");
+        System.out.println(JSONObject.toJSONString(map));
+        System.out.println(UUID.randomUUID().toString());
     }
-
-    public static int[] divingBoard(int shorter, int longer, int k) {
-        if(k == 0){
-            return new int[0];
-        }
-
-        Set<Integer> set = new TreeSet();
-        for(int i = 0; i <= k; k--){
-            set.add(shorter * (k-i) + i * longer);
-        }
-        int[] res = new int[set.size()];
-        int i = 0;
-        for(Integer val : set){
-            res[i++] = val;
-        }
-
-        return res;
-    }
-
 }
